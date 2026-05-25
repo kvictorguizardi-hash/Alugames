0c1e988207f9027ca405f75c186e94a100b93fdd
Victor Guizardi <kvictorguizardi@gmail.com>
2026-05-25 13:07:29 -0300

    feat: conclusao da logica de aluguel e devolucao

---
+++ alugames/js/app.js
@@ -0,0 +1,20 @@
+function alterarStatus(id) {
+    let jogoClicado = document.getElementById(`game-${id}`);
+    let imagem = jogoClicado.querySelector('.dashboard__item__img');
+    let botao = jogoClicado.querySelector('.dashboard__item__button');
+
+    if (botao.classList.contains('dashboard__item__button--return')) {
+        
+   
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+        
+    } else {
+        
+    
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+    }
+}
