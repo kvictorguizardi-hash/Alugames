993942e86e1e407ef560a9b25a8e5d2cb96f728d
Victor Guizardi <kvictorguizardi@gmail.com>
2026-05-25 14:14:12 -0300

    feat: adicionada confirmacao e contador de jogos

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -4,17 +4,31 @@ function alterarStatus(id) {
     let botao = jogoClicado.querySelector('.dashboard__item__button');
 
     if (botao.classList.contains('dashboard__item__button--return')) {
-        
-   
-        imagem.classList.remove('dashboard__item__img--rented');
+       if (confirm ('Você tem certeza que deseja devolver o jogo?')) {
+
+            imagem.classList.remove('dashboard__item__img--rented');
         botao.classList.remove('dashboard__item__button--return');
         botao.textContent = 'Alugar';
+
+       }
+    
         
     } else {
         
-    
-        imagem.classList.add('dashboard__item__img--rented');
+    if (confirm ('Você tem certeza que deseja alugar o jogo?')) {
+
+         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         botao.textContent = 'Devolver';
-    }
+
+        contarJogosAlugados ()
+    }}}
+
+function contarJogosAlugados() {
+   
+     let botoesAlugados = document.querySelectorAll('.dashboard__item__button--return');
+    let totalAlugados = botoesAlugados.length;
+    
+    console.log(`Total de jogos alugados agora: ${totalAlugados}`);
 }
+
